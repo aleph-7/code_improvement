bd3f170c9ec582caa7e0b9650745893e93258f9d
Arush <arush.up@gmail.com>
2024-03-26 03:59:21 +0530

    moved coach into coach.js

--- server/server.js
+++ server/server.js
@@ -52,6 +52,9 @@ app.use("/booking", bookingRoutes);
 //Counsellor Dashboard backend
 const CounsellorRoutes = require("./routes/counsellor");
 app.use("/counsellor", CounsellorRoutes);
+//coach backend
+const CoachRoutes = require("./routes/coach");
+app.use("/coach", CoachRoutes);
 const leaderboardRoutes = require("./routes/leaderboard");
 app.use("/leaderboard", leaderboardRoutes);
 const gymSwimmingRoutes = require("./routes/gyminstuctor");
@@ -107,135 +110,6 @@ app.post("/yoga/postSession", async (req, res) => {
     //Sending the error message to the frontend
     console.log(err);
     res.status(500).json({ error: "Post failed" });
-  }
-});
-
-app.post("/coach/postWorkshop", async (req, res) => {
-  try {
-    const new_workshop = new Workshop({
-      time_slot_start: req.body.start_time,
-      time_slot_end: req.body.end_time,
-      content: req.body.description,
-      // equipment: req.body.equipment,
-      equipment: {
-        raquet: Number(req.body.raquet),
-        cork: Number(req.body.cork),
-        shoe: Number(req.body.shoe),
-      },
-      coach_user_id: req.body.coach_user_id,
-      max_strength: req.body.max_participants,
-      date_slot: req.body.date,
-      participant_id: [],
-      type_of_sport: req.body.type_of_sport,
-    });
-    console.log(new_workshop);
-    const doc = await new_workshop.save();
-    //Sending the response to the frontend
-    res.status(200).json({ message: "Post successful" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Post failed" });
-  }
-});
-
-app.post("/coach/postWorkshop", async (req, res) => {
-  try {
-    const new_workshop = new Workshop({
-      time_slot_start: req.body.start_time,
-      time_slot_end: req.body.end_time,
-      content: req.body.description,
-      // equipment: req.body.equipment,
-      equipment: {
-        raquet: Number(req.body.raquet),
-        cork: Number(req.body.cork),
-        shoe: Number(req.body.shoe),
-      },
-      coach_user_id: req.body.coach_user_id,
-      max_strength: req.body.max_participants,
-      date_slot: req.body.date,
-      participant_id: [],
-      type_of_sport: req.body.type_of_sport,
-    });
-    console.log(new_workshop);
-    const doc = await new_workshop.save();
-    //Sending the response to the frontend
-    res.status(200).json({ message: "Post successful" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Post failed" });
-  }
-});
-
-app.post("/coach/postWorkshop", async (req, res) => {
-  try {
-    const new_workshop = new Workshop({
-      time_slot_start: req.body.start_time,
-      time_slot_end: req.body.end_time,
-      content: req.body.description,
-      // equipment: req.body.equipment,
-      equipment: {
-        raquet: Number(req.body.raquet),
-        cork: Number(req.body.cork),
-        shoe: Number(req.body.shoe),
-      },
-      coach_user_id: req.body.coach_user_id,
-      max_strength: req.body.max_participants,
-      date_slot: req.body.date,
-      participant_id: [],
-      type_of_sport: req.body.type_of_sport,
-    });
-    console.log(new_workshop);
-    const doc = await new_workshop.save();
-    //Sending the response to the frontend
-    res.status(200).json({ message: "Post successful" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Post failed" });
-  }
-});
-
-app.get("/coach/statistics", async (req, res) => {
-  try {
-    let attributeList;
-    const sport = req.query.type_of_sport;
-    // Assuming the coach_user_id is passed as a query parameter
-    // Retrieve workshops associated with the specified coach_user_id
-    await Workshop.find({ type_of_sport: sport }).then((results) => {
-      attributeList = results.map((doc) => ({
-        content: doc.content,
-        participantsCount: doc.participants_id.length, // Get the length of participants_id array
-      }));
-    });
-
-    console.log(attributeList);
-
-    // Sending the retrieved workshops as a response to the frontend
-    res.status(200).json({ attributeList });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Failed to retrieve workshops" });
-  }
-});
-
-app.post("/coach/reserveCourt", async (req, res) => {
-  try {
-    const new_reservation = new SportsBookings({
-      time_slot: req.body.time_slot,
-      date: req.body.date_slot,
-      court_id: req.body.court_id,
-      show_up_status: req.body.show_up_status,
-      type_of_sport: req.body.type_of_sport,
-      time_of_booking: req.body.time_of_booking,
-      booking_status: req.body.booking_status,
-      user_id: req.body.user_id,
-    });
-    console.log(new_reservation);
-    const doc = await new_reservation.save();
-    //Sending the response to the frontend
-    res.status(200).json({ message: "Reservation successful" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Reservation failed" });
   }
 });
 
