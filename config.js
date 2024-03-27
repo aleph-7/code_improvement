9456e9a91f200e4feb8f68d2b22798b6ceccbe5c
Kushagra Srivastava <kushagra@Kushagras-MacBook-Pro.local>
2024-03-28 03:33:19 +0530

    updates

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 // config.js
-const SERVER_ROOT_PATH = "https://elysium-231i.onrender.com";
+// const SERVER_ROOT_PATH = "https://elysium-231i.onrender.com";
+
+const SERVER_ROOT_PATH =
+  window.location.hostname === "localhost"
+    ? "http://localhost:6300"
+    : "https://elysium-231i.onrender.com";
 
 export default SERVER_ROOT_PATH;
