ff4682b05b6e25f333091e0d2cbd3d61a84ce5d2
Kushagra Srivastava <rockin.numbers@gmail.com>
2024-03-31 16:53:43 +0530

    overlap counsellor fixed

--- src/Dashboard/Counsellor/screens/acceptAppointments/acceptAppointments.jsx
+++ src/Dashboard/Counsellor/screens/acceptAppointments/acceptAppointments.jsx
@@ -24,39 +24,36 @@ const AcceptAppointments = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-        try {
-            const response = await fetch(SERVER_ROOT_PATH + "/counsellor/getAppointments", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    counsellor_user_id: localStorage.getItem("userMongoId"),
-                }),
-            });
-            const data = await response.json();
-            setMessage(data.message);
-            SetUpcomingAppointments(data.message.filter(msg => msg.booking_status === 1));
-            SetPendingAppointments(data.message.filter(msg => msg.booking_status === 0));
-        } catch (err) {
-            console.log(err);
-        }
+      try {
+        const response = await fetch(
+          SERVER_ROOT_PATH + "/counsellor/getAppointments",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              counsellor_user_id: localStorage.getItem("userMongoId"),
+            }),
+          }
+        );
+        const data = await response.json();
+        setMessage(data.message);
+        SetUpcomingAppointments(
+          data.message.filter((msg) => msg.booking_status === 1)
+        );
+        SetPendingAppointments(
+          data.message.filter((msg) => msg.booking_status === 0)
+        );
+      } catch (err) {
+        console.log(err);
+      }
     };
 
     fetchData();
-}, []);
-  const onClickButton = async (isAccept, currAppointment) => {
-    // isAccept is 1 for accepting and -1 for rejecting
-    // here if the appointment is being accepted, we reject the other appointments
-    if (isAccept === 1) {
-      pendingAppointments.forEach(appointment => {
-        if (appointment.date_slot === currAppointment.date_slot && appointment.time_slot === currAppointment.time_slot) {
-            // Update the appointment as needed
-            onClickButton(-1,appointment);
-        }
-      });
-    }
-    console.log("button pressed");
+  }, []);
+
+  const sendData = async (isAccept, Appointment) => {
     try {
       const response = await fetch(
         SERVER_ROOT_PATH + "/counsellor/acceptAppointments",
@@ -68,7 +65,7 @@ const AcceptAppointments = () => {
           },
           body: JSON.stringify({
             isAccept: isAccept,
-            appointment_id: currAppointment.booking_id,
+            appointment_id: Appointment.booking_id,
           }),
         }
       );
@@ -77,13 +74,77 @@ const AcceptAppointments = () => {
         throw new Error("Network response was not ok");
       }
       const data = await response.json();
-      return data;
+      console.log(data);
       // Assuming your backend returns a response indicating success or failure
     } catch (error) {
       console.error("Error accpeting/rejecting:", error);
       throw error;
     }
   };
+
+  const onClickButton = async (isAccept, currAppointment) => {
+    // isAccept is 1 for accepting and -1 for rejecting
+    // here if the appointment is being accepted, we reject the other appointments
+    if (isAccept === 1) {
+      sendData(1, currAppointment);
+      pendingAppointments.forEach((appointment) => {
+        if (
+          appointment.date_slot === currAppointment.date_slot &&
+          appointment.time_slot === currAppointment.time_slot &&
+          appointment.booking_id !== currAppointment.booking_id
+        ) {
+          // Update the appointment as needed
+          sendData(-1, appointment);
+        }
+      });
+      console.log("button pressed");
+    } else {
+      sendData(-1, currAppointment);
+      console.log("button pressed");
+    }
+  };
+  // const onClickButton = async (isAccept, currAppointment) => {
+  //   // isAccept is 1 for accepting and -1 for rejecting
+  //   // here if the appointment is being accepted, we reject the other appointments
+  //   if (isAccept === 1) {
+  //     pendingAppointments.forEach((appointment) => {
+  //       if (
+  //         appointment.date_slot === currAppointment.date_slot &&
+  //         appointment.time_slot === currAppointment.time_slot
+  //       ) {
+  //         // Update the appointment as needed
+  //         onClickButton(-1, appointment);
+  //       }
+  //     });
+  //   }
+  //   console.log("button pressed");
+  //   try {
+  //     const response = await fetch(
+  //       SERVER_ROOT_PATH + "/counsellor/acceptAppointments",
+  //       {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           // You may need to include additional headers if required by your backend
+  //         },
+  //         body: JSON.stringify({
+  //           isAccept: isAccept,
+  //           appointment_id: currAppointment.booking_id,
+  //         }),
+  //       }
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error("Network response was not ok");
+  //     }
+  //     const data = await response.json();
+  //     return data;
+  //     // Assuming your backend returns a response indicating success or failure
+  //   } catch (error) {
+  //     console.error("Error accpeting/rejecting:", error);
+  //     throw error;
+  //   }
+  // };
 
   return (
     <div className="Acceptappointments">
